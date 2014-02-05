bf73398715ab2fbfa7da0e2cb1a3538fca88f7bd
Robert Giesecke <robert.giesecke@gmail.com>
2014-02-05 11:07:43 +0100

    changed DebuggerDisplayInjector.AddDebuggerDisplayAttributes to pick up inherited properties and fields (and to skip properties with parameters)

--- Fody/DebuggerDisplayInjector.cs
+++ Fody/DebuggerDisplayInjector.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using Mono.Cecil;
@@ -9,10 +10,19 @@ public static class DebuggerDisplayInjector
         if (type.IsEnum || type.CustomAttributes.Any(c => c.AttributeType.Name == "CompilerGeneratedAttribute" || c.AttributeType.Name == "DebuggerDisplayAttribute"))
             return;
 
-        var fields = type.Fields.Where(f => f.IsPublic && CanPrint(f.FieldType)).Cast<MemberReference>();
-        var props = type.Properties.Where(p => p.GetMethod != null && p.GetMethod.IsPublic && CanPrint(p.PropertyType)).Cast<MemberReference>();
+        var members = (from t in EnumerateBaseTypes(type)
+                       let fields = t.Fields.Where(f => f.IsPublic && f.DeclaringType == t && CanPrint(f.FieldType))
+                       let props = from p in t.Properties
+                                   where p.DeclaringType == t
+                                   && p.GetMethod != null && p.GetMethod.IsPublic
+                                   && !p.HasParameters
+                                   && CanPrint(p.PropertyType)
+                                   select p
+                       from m in fields.Cast<MemberReference>().Concat(props)
+                       group m by m.Name into g
+                       select g.First()).ToList();
 
-        var displayBits = fields.Concat(props)
+        var displayBits = members
             .OrderBy(m => m.Name)
             .Select(m => string.Format("{0} = {{{0}}}", m.Name));
 
@@ -22,6 +32,23 @@ public static class DebuggerDisplayInjector
             debuggerDisplay.ConstructorArguments.Add(new CustomAttributeArgument(moduleDefinition.TypeSystem.String, string.Join(" | ", displayBits)));
             type.CustomAttributes.Add(debuggerDisplay);
         }
+    }
+
+    private static IEnumerable<TypeDefinition> EnumerateBaseTypes(TypeDefinition typeDefinition)
+    {
+        if (IsDelegate(typeDefinition) || typeDefinition.IsEnum || typeDefinition.IsInterface)
+            yield break;
+
+        while (typeDefinition.BaseType != null)
+        {
+            yield return typeDefinition;
+            typeDefinition = typeDefinition.BaseType.Resolve();
+        }
+    }
+
+    private static bool IsDelegate(TypeDefinition typeDefinition)
+    {
+        return typeDefinition.BaseType != null && typeDefinition.BaseType.FullName == typeof(MulticastDelegate).FullName;
     }
 
     private readonly static HashSet<string> basicNames = new HashSet<string>
